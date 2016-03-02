92a6654b02031ebe2591b65d39e2bc5974a2cc1e
Daesun Yim <yimdaesun@gmail.com>
2016-03-01 23:48:51 -0800

    Seller validations

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,2 +1,7 @@
 class Seller < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :subscription_plan, presence: true
+  validates :is_credit_card_linked, :inclusion => {:in => [true, false]}
 end
